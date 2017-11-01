97718d453acad668d1f612350a1a43b8c56a77b3
JTP709 <JTP709@gmail.com>
2017-10-31 21:29:04 -0400

    finished about me and updated projects css

--- model/model.py
+++ model/model.py
@@ -3,7 +3,8 @@ projects = [
 		'id': 1,
 		'title': 'Simple Safety Manager',
 		'description': 'A safety compliance program built using a Python backend with Flask framework, PostgreSQL database, and Bootstrap front-end.',
-		'image': 'http://via.placeholder.com/250x150',
+		'thumb_img': 'http://via.placeholder.com/250x250',
+		'modal_img': 'http://via.placeholder.com/500x500',
 		'github': '',
 		'demo': ''
 	},
@@ -11,15 +12,17 @@ projects = [
 		'id': 2,
 		'title': 'Emergency Event Manager',
 		'description': 'An event management app built using the Knockout.js framework and Google Maps JavaScript API.',
-		'image': 'http://via.placeholder.com/250x150',
+		'thumb_img': 'http://via.placeholder.com/250x250',
+		'modal_img': 'http://via.placeholder.com/500x500',
 		'github': '',
 		'demo': ''
 	},
 	{
-		'id': 2,
+		'id': 3,
 		'title': 'Final Project Placeholder',
 		'description': 'A to be determined program built using a Python backend (probably) and JavaScript (frontend).',
-		'image': 'http://via.placeholder.com/250x150',
+		'thumb_img': 'http://via.placeholder.com/250x250',
+		'modal_img': 'http://via.placeholder.com/500x500',
 		'github': '',
 		'demo': ''
 	}
