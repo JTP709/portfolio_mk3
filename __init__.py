01835766b3b89b894a9e1601cbcc01034334b0bf
Jon Prell <JTP709@gmail.com>
2017-11-03 14:16:58 -0400

    filename changes for server deployement

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask import make_response, flash, session
 from flask_mail import Mail, Message
 from model.model import projects
-from model.email_config import email_user, email_pass, email_recip
+from model.email_config import email_user, email_pass, email_recip, super_secret_key
 
 app = Flask(__name__)
 
@@ -39,7 +39,7 @@ def main():
                                projects=projects)
 
 if __name__ == '__main__':
-    app.secret_key = 'super secret key'
+    app.secret_key = super_secret_key
     app.config['SESSION_TYPE'] = 'filesystem'
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
