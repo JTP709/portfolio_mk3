e89fcdd3130eb880299990eb25de84c15ccafedd
Jon.Prell <JTP709@gmail.com>
2018-01-05 13:42:46 -0500

    updated projects

--- model/model.py
+++ model/model.py
@@ -1,16 +1,16 @@
 projects = [
 	{
 		'id': 1,
-		'title': 'Simple Safety Manager',
-		'description': 'A safety compliance program built using a Python backend with Flask framework, PostgreSQL database, and Bootstrap front-end.',
-		'thumb_img': '/static/img/SSM_desktop.gif',
-		'desktop_img': '/static/img/SSM_desktop.gif',
-		'tablet_img': '/static/img/SSM_tablet.gif',
-		'phone_img': '/static/img/SSM_phone.gif',
-		'github': 'https://github.com/JTP709/SMS',
+		'title': 'Annie Lou\'s Pet Sitting Service',
+		'description': 'A single page applicatin developed using React.js for a professional Pet Sitting Service. In addition to React.js, this app uses React-Router for declarative routing and React Bootstrap for site responsivness. This site was developed in collaboration with Steven Dilbrert.',
+		'thumb_img': '/static/img/JP_desktop.gif',
+		'desktop_img': '/static/img/JP_desktop.gif',
+		'tablet_img': '/static/img/JP_tablet.gif',
+		'phone_img': '/static/img/JP_phone.gif',
+		'github': 'https://github.com/JTP709/annieloupss',
 		'demo': None,
-		'front_end': ['HTML5','CSS3','JavaScript','Bootstrap'],
-		'back_end': ['Python3','Flask','SQLAlchemy','PostgreSQL']
+		'front_end': ['React.js', 'React-Router', 'Bootsrap'],
+		'back_end': ['Node.js']
 	},
 	{
 		'id': 2,
@@ -21,22 +21,22 @@ projects = [
 		'tablet_img': '/static/img/EEM_tablet.gif',
 		'phone_img': '/static/img/EEM_phone.gif',
 		'github': 'https://github.com/JTP709/Emergency-Event-Manager',
-		'demo': None,
+		'demo': 'https://jtp709.github.io/Emergency-Event-Manager/',
 		'front_end': ['HTML5','CSS3','JavaScript'],
 		'back_end': ['Knockout.js']
 	},
 	{
 		'id': 3,
-		'title': 'Annie Lou\'s Pet Sitting Service',
-		'description': 'A single page applicatin developed using React.js for a professional Pet Sitting Service. In addition to React.js, this app uses React-Router for declarative routing, React Bootstrap for site responsivness, and React-Router-Bootstrap for integrating routing with Bootsrap\'s navigation features. This site was developed in collaboration with <a href="https://github.com/sdilby">Steven Dilbrert</a>',
-		'thumb_img': '/static/img/JP_desktop.gif',
-		'desktop_img': '/static/img/JP_desktop.gif',
-		'tablet_img': '/static/img/JP_tablet.gif',
-		'phone_img': '/static/img/JP_phone.gif',
-		'github': 'https://github.com/JTP709/annieloupss',
+		'title': 'Simple Safety Manager',
+		'description': 'A safety compliance program built using a Python backend with Flask framework, PostgreSQL database, and Bootstrap front-end.',
+		'thumb_img': '/static/img/SSM_desktop.gif',
+		'desktop_img': '/static/img/SSM_desktop.gif',
+		'tablet_img': '/static/img/SSM_tablet.gif',
+		'phone_img': '/static/img/SSM_phone.gif',
+		'github': 'https://github.com/JTP709/SMS',
 		'demo': None,
-		'front_end': ['HTML5','CSS3','JavaScript','React.js', 'React-Router', 'React-Bootsrap', 'React-Router-Bootstrap'],
-		'back_end': ['Node.js']
+		'front_end': ['HTML5','CSS3','JavaScript','Bootstrap'],
+		'back_end': ['Python3','Flask','SQLAlchemy','PostgreSQL']
 	},
 	{
 		'id': 4,
