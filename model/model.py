273057e7b4cccf00d46e0a0ea28d73b0cf8eae0d
Jon Prell <JTP709@gmail.com>
2018-01-05 15:24:58 -0500

    added annieloupss project

--- model/model.py
+++ model/model.py
@@ -1,6 +1,32 @@
 projects = [
 	{
 		'id': 1,
+		'title': 'Annie Lou\'s Pet Sitting Service',
+		'description': 'A single page applicatin developed using React.js for a professional Pet Sitting Service. In addition to React.js, this app uses React-Router for declarative routing and React Bootstrap for site responsivness. This site was developed in collaboration with Steven Dilbert.',
+		'thumb_img': '/static/img/ALP_desktop.gif',
+		'desktop_img': '/static/img/ALP_desktop.gif',
+		'tablet_img': '/static/img/ALP_tablet.gif',
+		'phone_img': '/static/img/ALP_phone.gif',
+		'github': 'https://github.com/JTP709/annieloupss',
+		'demo': None,
+		'front_end': ['React.js', 'React-Router', 'Bootsrap'],
+		'back_end': ['Node.js']
+	},
+	{
+		'id': 2,
+		'title': 'Emergency Event Manager',
+		'description': 'An event management app built using the Knockout.js framework and Google Maps JavaScript API.',
+		'thumb_img': '/static/img/EEM_desktop.gif',
+		'desktop_img': '/static/img/EEM_desktop.gif',
+		'tablet_img': '/static/img/EEM_tablet.gif',
+		'phone_img': '/static/img/EEM_phone.gif',
+		'github': 'https://github.com/JTP709/Emergency-Event-Manager',
+		'demo': 'https://jtp709.github.io/Emergency-Event-Manager/',
+		'front_end': ['HTML5','CSS3','JavaScript'],
+		'back_end': ['Knockout.js']
+	},
+	{
+		'id': 3,
 		'title': 'Simple Safety Manager',
 		'description': 'A safety compliance program built using a Python backend with Flask framework, PostgreSQL database, and Bootstrap front-end.',
 		'thumb_img': '/static/img/SSM_desktop.gif',
@@ -13,20 +39,7 @@ projects = [
 		'back_end': ['Python3','Flask','SQLAlchemy','PostgreSQL']
 	},
 	{
-		'id': 2,
-		'title': 'Emergency Event Manager',
-		'description': 'An event management app built using the Knockout.js framework and Google Maps JavaScript API.',
-		'thumb_img': '/static/img/EEM_desktop.gif',
-		'desktop_img': '/static/img/EEM_desktop.gif',
-		'tablet_img': '/static/img/EEM_tablet.gif',
-		'phone_img': '/static/img/EEM_phone.gif',
-		'github': 'https://github.com/JTP709/Emergency-Event-Manager',
-		'demo': None,
-		'front_end': ['HTML5','CSS3','JavaScript'],
-		'back_end': ['Knockout.js']
-	},
-	{
-		'id': 3,
+		'id': 4,
 		'title': 'Portfolio Site',
 		'description': 'My personal porftolio site and blog. The site is deployed using a Python-Flask back end with a custom front-end using JavaScript and jQuery.',
 		'thumb_img': '/static/img/JP_desktop.gif',
